d7df42a94a9270cd84c85f4a5eada9f157ff65a0
Pawel Sikorski <pawel.sikorski@cdprojektred.com>
2022-12-12 17:34:13 +0100

    day12 part2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,5 +1,5 @@
-//use pathfinding::dijkstra;
 use pathfinding::prelude::bfs;
+use std::cmp::min;
 
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Node {
@@ -22,10 +22,10 @@ impl Node {
         if curr == 'S' as u8 {
             curr = 'a' as u8
         }
-        println!("iter: ({}, {}) -> ({}, {})", x, y, end.x, end.y);  
-        if x == end.x && y == end.y {
-            println!("FOUND: {}, {}", end.x, end.y);  
-        }
+        //println!("iter: ({}, {}) -> ({}, {})", x, y, end.x, end.y);  
+        //if x == end.x && y == end.y {
+          //  println!("FOUND: {}, {}", end.x, end.y);  
+        //}
         //println!("SUCCESSORS: curr {}x{} = {curr} | length {length}, height {height}", x, y);
         let mut right = 0;
         if x+1 < length {
@@ -59,36 +59,36 @@ impl Node {
         //println!("current {curr}, > {right}, < {left}, ^ {top}, v {bottom}");
         if x+1 < length && right as i32 - curr  as i32 <= 1 { // >
             result.push(Node{x: x+1, y: y});
-            if x+1 == end.x && y == end.y {
-                println!("FOUND: > {}, {}", end.x, end.y);  
-            }
+           // if x+1 == end.x && y == end.y {
+         //       println!("FOUND: > {}, {}", end.x, end.y);  
+         //   }
             //println!("SUCCESSORS: > {}, {}", x+1, y);
         }
         //println!("right");
         if x-1>=0 && left as i32 - curr as i32 <= 1 { // <
             result.push(Node{x: x-1, y});
-            if x-1 == end.x && y == end.y {
-                println!("FOUND: < {}, {}", end.x, end.y);  
-            }
+            //if x-1 == end.x && y == end.y {
+              //  println!("FOUND: < {}, {}", end.x, end.y);  
+            //}
             //println!("SUCCESSORS: < {}, {}", x-1, y);
         }
         //println!("left");
         if y-1>=0 && top as i32 - curr as i32 <= 1 { // ^
             result.push(Node{x: x, y: y-1});
             //println!("SUCCESSORS: ^ {}, {}", x, y-1);
-            if x == end.x && y-1 == end.y {
-                println!("FOUND: ^ {}, {}", end.x, end.y);  
-            }
+           // if x == end.x && y-1 == end.y {
+              //  println!("FOUND: ^ {}, {}", end.x, end.y);  
+           // }
         }
         //println!("top");
         if y+1 < height && bottom as i32 - curr as i32 <= 1 { // V
             result.push(Node{x: x, y: y+1});
             //println!("SUCCESSORS: ^ {}, {}", x, y+1);
-            if x == end.x && y+1 == end.y {
-                println!("FOUND: v {}, {}", end.x, end.y);  
-            }
+           // if x == end.x && y+1 == end.y {
+           //     println!("FOUND: v {}, {}", end.x, end.y);  
+          //  }
         }
-        println!("SUCCESSORS of ({},{}): all {}: {:?}", x, y, result.len(), result);
+        //println!("SUCCESSORS of ({},{}): all {}: {:?}", x, y, result.len(), result);
         result
     }
 }
@@ -101,34 +101,43 @@ fn solve_1(input: &str) -> usize {
     let lines = input.lines();
     let height = lines.count() as i32;
     let length = (input.find('\n').unwrap() - 1) as i32;
-    //let length_with_white = length + 2;
-    println!("W{length}xH{height}");
+    //println!("W{length}xH{height}");
     
     let removed_whitespaces = remove_whitespace(input);
-    println!("size orig {} noWh {}", input.len(), removed_whitespaces.len());
+    //println!("size orig {} noWh {}", input.len(), removed_whitespaces.len());
 
     let raw_bytes = removed_whitespaces.as_bytes();
-    let s_id = raw_bytes.iter().position(|&i| i == 'S' as u8).unwrap() as i32;
     let e_id = raw_bytes.iter().position(|&i| i == 'E' as u8).unwrap() as i32;
-    //std::mem::replace(&mut raw_bytes[s_id as usize], 'a' as u8);
-    let start = Node{x: s_id%length, y: s_id / length};
     let end = Node{x: e_id%length, y: e_id / length};
-    println!("start s_id{s_id} ({}x{}), end e_id{e_id} ({}x{})", start.x, start.y, end.x, end.y);
 
-    let result = bfs(&start, |p| p.successors(raw_bytes, length, height, &end), |p| *p == end);
-    let mut ret = 0;
-    if result.is_none() {
-        println!("NONE");
-    }
-    else {
-        let res_unwrapped = result.unwrap();
-        println!("SOME: size {}", res_unwrapped.len());
-        ret = res_unwrapped.len() - 1;
-        for r in res_unwrapped {
-            println!("N {}x{}", r.x, r.y);
+    let mut min_path = std::usize::MAX;
+    for s_id in 0..raw_bytes.len() {
+        if raw_bytes[s_id] == 'a' as u8 || raw_bytes[s_id] == 'S' as u8 {
+
+            //let s_id = raw_bytes.iter().position(|&i| i == 'S' as u8).unwrap() as i32;
+            let s_id_conv = s_id as i32;
+            let start = Node{x: s_id_conv%length, y: s_id_conv / length};
+            println!("start s_id{s_id} ({}x{}), end e_id{e_id} ({}x{})", start.x, start.y, end.x, end.y);
+            let result = bfs(&start, |p| p.successors(raw_bytes, length, height, &end), |p| *p == end);
+            let mut ret = 0;
+            if result.is_none() {
+                println!("NONE");
+            }
+            else {
+                let res_unwrapped = result.unwrap();
+                println!("SOME: size {}", res_unwrapped.len());
+                ret = res_unwrapped.len() - 1;
+                min_path = min(min_path, ret);
+                //for r in res_unwrapped {
+                // println!("N {}x{}", r.x, r.y);
+                //}
+            }
+        }
+        else {
+            continue;
         }
     }
-    ret
+    min_path
 }
 
 fn main() {
