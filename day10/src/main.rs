0d1f5189d3a3cb05180f8a4dc49dcea83d751497
Pawel Sikorski <pawel.sikorski@cdprojektred.com>
2022-12-12 18:14:27 +0100

    day10 part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -35,27 +35,54 @@ fn solve_1(input: &str) -> i32 {
     values.push(RegAtCycle{cycle: 180, register: 0});
     values.push(RegAtCycle{cycle: 220, register: 0});
     let lines = input.lines();
+    let mut pixel_buffer = vec!['.' as u8; 40];
     for line in lines {
         cycle += 1;
+        let mut pix_number = (cycle - 1) % 40;
+        if (register as i32 - pix_number).abs() <= 1 {
+           // println!(">> cycle {cycle}, pix_number {pix_number}, register {register}");
+            pixel_buffer[pix_number as usize] = '#' as u8;
+        }
+        else {
+           // println!("cycle {cycle}, pix_number {pix_number}, register {register}");
+        }
+        if cycle % 40 == 0 {
+            let s = std::str::from_utf8(&pixel_buffer);
+            println!("{:?} , cycle {}", s, cycle);
+            pixel_buffer = vec!['.' as u8; 40];
+        }
         let i = is_wanted_cycle(cycle, &values);
         if i != -1 {
-            println!("FOUND+2 {cycle} cycle = {register}");
+           // println!("FOUND+2 {cycle} cycle = {register}");
             values[i as usize].register = register;
             //break;
         }
         
         if line == "noop" {
-            println!("cycle{cycle} noop, register {register}, i {i}");
+           // println!("cycle{cycle} noop, register {register}, i {i}");
             continue;
         }
         else {
             let a = parse_line(line);
-            println!("cycle{cycle}, register {register} addx {a}, i {i}");
+           // println!("cycle{cycle}, register {register} addx {a}, i {i}");
             
             cycle += 1;
+            pix_number = (cycle - 1) % 40;
+            if (register as i32 - pix_number as i32).abs() <= 1 {
+                //println!("> cycle {cycle}, pix_number {pix_number}, register {register}");
+                pixel_buffer[pix_number as usize] = '#' as u8;
+            }
+            else {
+                //println!("cycle {cycle}, pix_number {pix_number}, register {register}");
+            }
             let j = is_wanted_cycle(cycle, &values);
+            if cycle % 40 == 0 {
+                let s = std::str::from_utf8(&pixel_buffer);
+                println!("{:?} , cycle {}", s, cycle);
+                pixel_buffer = vec!['.' as u8; 40];
+            }
             if j >= 0 {
-                println!("FOUND+2 {cycle} cycle = {register}");
+               // println!("FOUND+2 {cycle} cycle = {register}");
                 values[j as usize].register = register;
                 //break;
             }
@@ -66,7 +93,7 @@ fn solve_1(input: &str) -> i32 {
     for v in values {
         let temp  = v.register*v.cycle;
         s += temp;
-        println!("SUM {s} {temp} = {}x{}", v.register, v.cycle);
+        //println!("SUM {s} {temp} = {}x{}", v.register, v.cycle);
     }
     s
     //values.iter().map(|&i| i.register as i32).sum()
