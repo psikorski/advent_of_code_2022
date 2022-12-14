c460de8364270a030b13cf7d3c271ce2ce99c24f
Pawel Sikorski <pawel.sikorski@cdprojektred.com>
2022-12-14 18:14:24 +0100

    skeletons for day13 and day14

---
+++ day14/src/main.rs
@@ -0,0 +1,12 @@
+fn solve_1(input: &str) -> usize {
+    let lines = input.lines();
+0
+}
+
+fn main() {
+    let input = include_str!("../input_sample.txt");
+    let result1 = solve_1(input);
+    println!("result1 = {result1}");
+    //let result2 = solve_2(input);
+    //println!("result2 = {result2}");
+}
