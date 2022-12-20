75f0fea0d9c8ed2f6c6fa504d5b6b3379327eb53
Pawel Sikorski <email@gmail.com>
2022-12-20 16:16:45 +0100

    day13 part2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,4 +1,5 @@
 use std::cmp::min;
+use std::cmp::Ordering;
 
 #[derive(Debug)]
 struct Element{
@@ -8,6 +9,17 @@ struct Element{
 impl Element {
     fn simple_val(val: i32) -> Self {
         Element{elements: Vec::new(), value: Some(val)}
+    }
+    fn copyy(self: &Element) -> Self {
+        let mut result = Element{elements: Vec::new(), value: Option::None};
+        if self.value.is_some() {
+            result.value = Some(self.value.unwrap());
+            return result
+        }
+        for el in &self.elements {
+            result.elements.push(Box::new(el.copyy()));
+        }
+        result
     }
 }
 
@@ -60,14 +72,17 @@ fn print_el(elem: &Element) {
     }
 }
 
-#[derive(Debug, PartialEq)]
-enum CmpResult{
-    LeftSmaller,
-    CONTINUE,
-    RightSmaller,
+
+fn compare(el1: &Element, el2: &Element) -> Ordering {
+    let mut mel1 = el1.copyy();
+    let mut mel2 = el2.copyy();
+    //println!("compare: \n\t{:?}\n\t{:?}", mel1, mel2);
+    let res = compare_internal(&mut mel1, &mut mel2);
+    //println!("{:?}", res);
+    res
 }
 
-fn compare(el1: &mut Element, el2: &mut Element) -> CmpResult {
+fn compare_internal(el1: &mut Element, el2: &mut Element) -> Ordering {
     let val1_b = el1.value.is_some();
     let val2_b = el2.value.is_some();
     
@@ -75,36 +90,39 @@ fn compare(el1: &mut Element, el2: &mut Element) -> CmpResult {
         let el1_v = el1.value.unwrap();
         let el2_v = el2.value.unwrap();
         if el1_v < el2_v {
-            return CmpResult::LeftSmaller
+            return Ordering::Less
         }
         else if el1_v > el2_v {
-            return CmpResult::RightSmaller
+            return Ordering::Greater
         }
-        return CmpResult::CONTINUE
+        return Ordering::Equal
     }
     else if val1_b && !val2_b {
         el1.elements.push(Box::new(Element::simple_val(el1.value.unwrap())));
         el1.value = Option::None;
-        return compare(el1, el2)
+        return compare_internal(el1, el2)
     }
     else if !val1_b && val2_b {
         el2.elements.push(Box::new(Element::simple_val(el2.value.unwrap())));
         el2.value = Option::None;
-        return compare(el1, el2)
+        return compare_internal(el1, el2)
     }
     else {
         let mut id = 0;
         let len1 = el1.elements.len();
         let len2 = el2.elements.len();
         loop {
+            if id >= len1 && id >= len2 {
+                return Ordering::Equal // FOR PART TWO
+            }
             if id >= len1 { //LEFT run out of items (RIGHT does not matter)
-                return CmpResult::LeftSmaller
+                return Ordering::Less
             }
             if id < len1 && id >= len2 { //RIGHT run out of items
-                return CmpResult::RightSmaller
+                return Ordering::Greater
             }
-            let cmp_res = compare(&mut el1.elements[id], &mut el2.elements[id]);
-            if cmp_res == CmpResult::CONTINUE {
+            let cmp_res = compare_internal(&mut el1.elements[id], &mut el2.elements[id]);
+            if cmp_res == Ordering::Equal {
                 id += 1;
                 continue
             }
@@ -124,16 +142,14 @@ fn solve_1(input: &str) -> usize {
     let mut result = 0;
     while pair_id < lines_len {
         let mut id = 1;
-        let mut el1 = read_el(&lines_vec[3*pair_id], &mut id);
+        let el1 = read_el(&lines_vec[3*pair_id], &mut id);
         id = 1;
-        //print_el(&el1);
-        let mut el2 = read_el(&lines_vec[3*pair_id+1], &mut id);
-        //print_el(&el2);
+        let el2 = read_el(&lines_vec[3*pair_id+1], &mut id);
         pair_id+=1; // TODO consider replacing with for loop
-        let cmp_res = compare(&mut el1, &mut el2);
+        let cmp_res = compare(&el1, &el2);
         match cmp_res {
-            CmpResult::LeftSmaller => result+=pair_id,
-            CmpResult::CONTINUE => result+=pair_id,
+            Ordering::Less => result+=pair_id,
+            Ordering::Equal => result+=pair_id,
             _=>(),
         }
         println!("{pair_id} el1 < el2 {:?}", cmp_res);
@@ -141,9 +157,44 @@ fn solve_1(input: &str) -> usize {
     result
 }
 
+fn solve_2(input: &str) -> usize {
+    let lines_vec: Vec<&str> = input.lines().collect();
+    let lines_len = (lines_vec.len()+1)/3;
+
+    let mut elements: Vec<Element> = Vec::new();
+
+    let mut pair_id = 0;
+
+    let mut result = 0;
+    while pair_id < lines_len {
+        let mut id = 1;
+        let el1 = read_el(&lines_vec[3*pair_id], &mut id);
+        id = 1;
+        let el2 = read_el(&lines_vec[3*pair_id+1], &mut id);
+        pair_id+=1; // TODO consider replacing with for loop
+        elements.push(el1);
+        elements.push(el2);
+    }
+    let mut id: usize = 1;
+    let raw_el2 = read_el("[[2]]", &mut id);
+    id = 1;
+    let raw_el6 = read_el("[[6]]", &mut id);
+    elements.push(raw_el2.copyy());
+    elements.push(raw_el6.copyy());
+
+    elements.sort_by(|a, b| compare(a, b));
+    //elements.iter().for_each(|x| println!("{:?}", x));
+
+    let p2 = elements.iter().position(|el| compare(el, &raw_el2) == Ordering::Equal);
+    let p6 = elements.iter().position(|el| compare(el, &raw_el6) == Ordering::Equal);
+    println!("FOUND: {:?}", p2);
+    println!("FOUND: {:?}", p6);
+    (p2.unwrap()+1)*(p6.unwrap()+1)
+}
+
 fn main() {
     let input = include_str!("../input.txt");
-    let result1 = solve_1(input);
+    let result1 = solve_2(input);
     println!("result1 = {result1}");
     //let result2 = solve_2(input);
     //println!("result2 = {result2}");
