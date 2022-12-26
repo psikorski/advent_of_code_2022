db7890fab3a2ad5c4d23dce7c97b9205617013ae
Pawel Sikorski <email@gmail.com>
2022-12-26 13:15:20 +0100

    day20 part 2

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -2,11 +2,11 @@ use sscanf::sscanf;
 
 pub struct Object {
     pub order: usize,
-    pub value: i32,
+    pub value: i64,
 }
 
 impl Object {
-    pub fn new(o: usize, v: i32) -> Self {
+    pub fn new(o: usize, v: i64) -> Self {
         Object{order: o, value: v}
     }
 }
@@ -15,12 +15,13 @@ type Objects = Vec<Object>;
 
 fn read_lines(input: &str) -> Objects {
     let mut values: Objects = Vec::new();
+    let mixer = 811589153;
     //let mut ordered: Objects = Vec::new();
     let lines = input.lines();
     let mut order = 0 as usize;
     for line in lines {
-        let parsed = sscanf!(line, "{}", i32);
-        values.push(Object::new(order, parsed.unwrap()));
+        let parsed = sscanf!(line, "{}", i64);
+        values.push(Object::new(order, parsed.unwrap()*mixer));
         //values.push(parsed.unwrap());
         //ordered.push(order);
         order += 1;
@@ -28,63 +29,83 @@ fn read_lines(input: &str) -> Objects {
     values
 }
 
-fn solve_1(input: &str) -> i32 {
+fn solve_1(input: &str) -> i64 {
     let mut values = read_lines(input);
     let mut values_placements: Vec<usize> = Vec::new();
     let len = values.len();
-    let leni = len as i32;
+    let leni = len as i64;
 
     for id in 0..len{
         values_placements.push(id);
     }
 
-    println!("Len {len}");
-    for id in 0..len {
-        println!("\nid == {id}, val {}", values[id].value);
-
-        let moves = values[id].value;
-        if moves == 0 {
-            // nothing
-            continue;
-        }
-
-        let current_placement = values[id].order as i32;
-        let mut new_id = current_placement + moves;
-        
-        let removed = values_placements.remove(current_placement as usize);        
-        for next_id in (current_placement as usize)..len-1 {
-            values[values_placements[next_id]].order = next_id;
-        }
-
-        //print!("new_id: ({new_id})\n\t");
-        if new_id == 0 {
-            //print!("new_id == 0\n");
-            new_id = leni -1;
-        }
-        else if new_id == leni - 1 {
-            //print!("new_id == leni -1\n");
-            println!("TODO??");
-        }
-        else if new_id < 0 {
-            while new_id < 0 {
-                //print!("new_id({new_id}) < 0");
-                new_id = leni - 1 + new_id;
-                print!("\t ==> new_id({new_id})\n");
-            }
-        }
-        else if new_id > leni - 1 { // TODO -1 or -2
-            while new_id > leni -1 {
-                //print!("new_id({new_id}) > len -1 ");
-                new_id = new_id - leni + 1;
-                print!("\t ==> new_id({new_id})\n");
-            }
-        }
-        values_placements.insert(new_id as usize, removed);
-
-        for next_id in new_id as usize..len {
-            values[values_placements[next_id]].order = next_id ;
-        } 
+    print!("Initial: ");
+    for v in &values {
+        print!("{} ", v.value);
     }
+    print!("\n");
+
+    println!("Len {len}");
+    for round in 0..10 {
+        for id in 0..len {
+            //println!("\nid == {id}, val {}", values[id].value);
+
+            let moves = values[id].value;
+            if moves == 0 {
+                // nothing
+                continue;
+            }
+
+            let current_placement = values[id].order as i64;
+            let mut new_id = current_placement + moves;
+            
+            let removed = values_placements.remove(current_placement as usize);        
+            for next_id in (current_placement as usize)..len-1 {
+                values[values_placements[next_id]].order = next_id;
+            }
+
+            new_id %= (leni-1);
+            //print!("new_id: ({new_id})\n\t");
+            if new_id == 0 {
+                //print!("new_id == 0\n");
+                new_id = leni -1;
+            }
+            else if new_id == leni - 1 {
+                //print!("new_id == leni -1\n");
+                println!("TODO??");
+            }
+            else if new_id < 0 {
+                //print!("new_id({new_id}) < 0");
+                //new_id %= (leni-1);
+                //while new_id < 0 {
+                    //print!(" => new_id({new_id}) < 0");
+                    new_id = leni - 1 + new_id;
+                    //print!("\t ==> new_id({new_id})\n");
+                //}
+            }
+            else if new_id > leni - 1 { // TODO -1 or -2
+                //print!("new_id({new_id}) > len -1 ");
+                //new_id %= (leni-1);
+                //while new_id > leni -1 {
+                    //print!(" => new_id({new_id}) > len -1 ");
+                    //new_id = new_id - leni + 1;
+                    //print!("\t ==> new_id({new_id})\n");
+                //}
+            }
+            values_placements.insert(new_id as usize, removed);
+
+            for next_id in new_id as usize..len {
+                values[values_placements[next_id]].order = next_id ;
+            } 
+        }
+
+        print!("After {round} round: ");
+        for v in &values_placements {
+            print!("{} ", values[*v].value);
+        }
+        print!("\n");
+    }
+
     let mut th1000 = 1000; 
     let mut th2000 = 2000;
     let mut th3000 = 3000;
