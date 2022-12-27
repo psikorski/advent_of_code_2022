8743d045d7e222188a8eef60c3844bdca7d02a0d
Pawel Sikorski <pawel.sikorski@cdprojektred.com>
2022-12-27 18:19:42 +0100

    day24 part1 and part2

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,34 +1,62 @@
 use dynamic_matrix::DynamicMatrix;
+use pathfinding::prelude::astar;
 
 type Ground = DynamicMatrix<char>;
 
-// move to another file TODO
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+// TODO print could be moved to common utils
+#[allow(dead_code)]
+fn print_matrix(ground: &Ground) {
+    let shape = ground.shape();
+    for y in 0..shape.0 {
+        print!("{y} ");
+        for x in 0..shape.1 {
+            print!("{}", ground[(y, x)]);
+        }
+        print!("\n");
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Position {
     pub x: i32,
     pub y: i32,
+    pub round: i32,
 }
 
-// move to another file TODO
 impl Position {
-    pub fn new(v_x: i32, v_y: i32) -> Self {
-        Self { x: v_x, y: v_y }
+    pub fn new(v_x: i32, v_y: i32, v_r: i32) -> Self {
+        Self { x: v_x, y: v_y, round: v_r }
     }
     pub fn def() -> Self {
-        Position::new(0,0)
+        Position::new(0,0, 0)
     }
     pub fn from_pair(pair: (usize, usize)) -> Self {
-        Position::new(pair.1 as i32, pair.0 as i32) // exchanged 0 with 1
+        Position::new(pair.1 as i32, pair.0 as i32, 0) // exchanged .0 with .1
     }
     pub fn pair(&self) -> (usize, usize) {
         (self.y as usize, self.x as usize)
     }
+    pub fn ux(&self) -> usize {
+        self.x as usize
+    }
+    pub fn uy(&self) -> usize {
+        self.y as usize
+    }
+    pub fn distance(&self, other: &Position) -> i32 {
+        (self.x.abs_diff(other.x) + self.y.abs_diff(other.y)) as i32
+    }
 }
 
-pub fn is_dir_move(dir: char) -> bool {
+pub fn is_dir_move_hor(dir: char) -> bool {
     match dir {
         '<' => true,
         '>' => true,
+        _ => false,
+    }
+}
+
+pub fn is_dir_move_ver(dir: char) -> bool {
+    match dir {
         '^' => true,
         'v' => true,
         _ => false,
@@ -45,36 +73,86 @@ pub fn dir_to_move(dir: char) -> i32 {
     }
 }
 
-struct Valley {
-    pub valley: Ground,
-    pub e: Position,
+pub fn possible_next(position: &Position, ground: Ground) -> Vec<(Position, i32)> {
+    //println!("possible_next for ({}x{}) round {}", position.ux(), position.uy(), position.round);
+    let mut result: Vec<(Position, i32)> = Vec::new();
+    // same place
+    if ground[position.pair()] == '.' {
+        result.push((Position::new(position.x, position.y, position.round+1), 1));
+    }
+    // top
+    if position.uy() > 0 && ground[(position.uy()-1, position.ux())] == '.' {
+        result.push((Position::new(position.x, position.y-1, position.round+1), 1));
+    }
+    // right
+    if ground[(position.uy(), position.ux()+1)] == '.' {
+        result.push((Position::new(position.x+1, position.y, position.round+1), 1));
+    }
+    // bottom
+    if position.uy() < ground.shape().0 - 1 && ground[(position.uy()+1, position.ux())] == '.' {
+        result.push((Position::new(position.x, position.y+1, position.round+1), 1));
+    }
+    // left
+    if ground[(position.uy(), position.ux()-1)] == '.' {
+        result.push((Position::new(position.x-1, position.y, position.round+1), 1));
+    }
+    result
 }
 
-impl Valley {
-    pub fn generate_next(&self, rounds: i32) -> Ground { //(Ground, Vec<Position>) {
-        let shape = self.valley.shape();
-        let mut next_valley = DynamicMatrix::with_capacity(shape);
+pub fn is_success(position: &Position, end: &Position) -> bool {
+    //println!("(is_success), round", )
+    if position.x == end.x && position.y == end.y {
+        return true
+    }
+    return false
+}
 
-        for y in 1..shape.0-1 {
-            for x in 1..shape.1-1 {
-                let current = self.valley[(y,x)];
-                if current == '.' {
-                    continue;
-                }
-                else if current == '<' {
-                    // use rounds to count how many left
-                    let mut new_x = x - rounds;
-                    //new_id %= (leni-1);
-                    //if new_id < 0 {
-                        //new_id = leni - 1 + new_id;
-                    //}
-                }
-                    
-                }
+pub fn count_new_pos(curr: i32, to_move: i32, len: i32) -> usize {
+    let mod_move = to_move % len;
+    let mut new_curr = curr + mod_move;
+    if new_curr < 1 {
+        new_curr = len + curr + mod_move;
+    }
+    else if new_curr > len {
+        new_curr = curr + mod_move - len;
+    }
+    new_curr as usize
+}
+
+pub fn generate_next(ground: &Ground, rounds: i32) -> Ground { //(Ground, Vec<Position>) {
+    //println!("(generate_next) round {rounds}");
+    let shape = ground.shape();
+    let x_in = shape.1 as i32 - 2;
+    let y_in = shape.0 as i32 - 2;
+    let mut next_valley = ground.clone();
+    for y in 1..shape.0-1 {
+        for x in 1..shape.1-1 {
+            next_valley[(y,x)] = '.';
+        }
+    }
+
+    for y in 1..shape.0-1 {
+        for x in 1..shape.1-1 {
+            let current = ground[(y,x)];
+            if current == '.' {
+                continue;
+            }
+            else if is_dir_move_hor(current) {
+                // x_in -2 czy -1 ?
+                let new_id = count_new_pos(x as i32, rounds * dir_to_move(current), x_in);
+                //println!("(generate_next) hor({x}x{y}), new_id{new_id}, rounds {rounds}, char {current}");
+                next_valley[(y,new_id)] = current;
+
+            }
+            else if is_dir_move_ver(current) {
+                // x_in -2 czy -1 ?
+                let new_id = count_new_pos(y as i32, rounds * dir_to_move(current), y_in);
+                //println!("(generate_next) ver({x}x{y}), new_id{new_id}, rounds {rounds}, char {current}");
+                next_valley[(new_id, x)] = current;
             }
         }
-        next_valley
     }
+    next_valley
 }
 
 // move to another file TODO
@@ -86,7 +164,7 @@ fn read_matrix_from_input(input: &str) -> Ground {
     let mut result = DynamicMatrix::with_capacity((height, width));
     
     for line in lines_vec {
-        let mut chars: Vec<char> = line.chars().map(|c| c as char).collect();
+        let chars: Vec<char> = line.chars().map(|c| c as char).collect();
         result.push_row(chars).unwrap();
     }
 
@@ -94,38 +172,66 @@ fn read_matrix_from_input(input: &str) -> Ground {
     result
 }
 
-fn find_start(ground: &Ground) -> Position {
-    let mut result = Position::new(1,0);
+fn find_start(_ground: &Ground) -> Position {
+    let result = Position::new(1,0, 0);
+    println!("START ({}x{})", result.x, result.y);
     result
 }
 
 fn find_end(ground: &Ground) -> Position {
     let (height, width) = ground.shape();
-    let mut result = Position::new(width as i32 - 2,height as i32 -1);
+    let result = Position::new(width as i32 - 2,height as i32 -1, 0);
+    println!("FINISH ({}x{})", result.x, result.y);
     result
 }
 
-// TODO print could be moved to common utils
-fn print_matrix(ground: &Ground) {
-    let shape = ground.shape();
-    for y in 0..shape.0 {
-        print!("{y} ");
-        for x in 0..shape.1 {
-            print!("{}", ground[(y, x)]);
-        }
-        print!("\n");
-    }
-}
-
 fn solve_1(input: &str) -> i32 {
-    let mut ground = read_matrix_from_input(input);
-    let mut elves = read_elves(&ground);
-    
-    0
+    let ground = read_matrix_from_input(input);
+    let mut start = find_start(&ground);
+    let mut finish = find_end(&ground);
+    print_matrix(&ground);
+    let result = astar(
+        &start,
+        |p| possible_next(p, generate_next(&ground, p.round + 1)),
+        |p| p.distance(&finish),
+                   |p| is_success(p, &finish)
+    );
+
+    let minutes_finish = result.unwrap().1;
+    // println!("MINUTES TO FINISH: {minutes_finish}");
+    //let ground_finish = generate_next(&ground, minutes_finish);
+    //print_matrix(&ground_finish);
+    finish.round = minutes_finish;
+    start.round = 0;
+    let result_to_start = astar(
+        &finish,
+        |p| possible_next(p, generate_next(&ground, p.round + 1)),
+        |p| p.distance(&start),
+                   |p| is_success(p, &start)
+    );
+    let minutes_to_start = result_to_start.unwrap().1;
+    println!("MINUTES TO START: {minutes_to_start}");
+    //let ground_finish_start = generate_next(&ground, minutes_finish + minutes_to_start);
+
+    finish.round = 0;
+    start.round = minutes_finish + minutes_to_start;    
+
+    let result_to_finish_start_finish = astar(
+        &start,
+        |p| possible_next(p, generate_next(&ground, p.round + 1)),
+        |p| p.distance(&finish),
+                   |p| is_success(p, &finish)
+    );
+    let minutes_to_finish_start_finish = result_to_finish_start_finish.unwrap().1;
+    println!("MINUTES TO FINISH-START-FINISH: {minutes_to_finish_start_finish}");
+
+
+
+    minutes_to_finish_start_finish + minutes_to_start + minutes_finish
 }
 
 fn main() {
-    let input = include_str!("../input_sample.txt");
+    let input = include_str!("../input.txt");
     let result1 = solve_1(input);
     println!("result1 = {result1}");
     //let result2 = solve_2(input);
